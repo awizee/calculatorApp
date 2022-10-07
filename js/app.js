349971c0c5b8361344a726e8b9490eb213c04475
awizee <muhammedawell@hotmail.com>
2022-10-07 23:28:23 +0100

    adding colors

--- js/app.js
+++ js/app.js
@@ -1,8 +1,45 @@
 const buttons = document.querySelectorAll("button");
 const display = document.querySelector("#display");
 
+let output = " ";
+let final = [];
+const symbol = [];
+
 for (let button of buttons) {
   button.addEventListener("click", (e) => {
-    display.innerText += e.target.innerText;
+    final = e.target.textContent;
+    final.push(output);
+
+    switch (e.target.textContent) {
+      case "+":
+        symbol.push("+");
+        output = " ";
+        console.log(output);
+        console.log(symbol);
+        break;
+      case "-":
+        symbol.push("-");
+        output = " ";
+        console.log(output);
+        console.log(symbol);
+        break;
+      case "*":
+        symbol.push("*");
+        output = " ";
+        console.log(output);
+        console.log(symbol);
+        break;
+      case "/":
+        symbol.push("/");
+        output = " ";
+        console.log(output);
+        console.log(symbol);
+        break;
+      case "=":
+        symbol.push("=");
+        console.log(output);
+        console.log(symbol);
+    }
+    display.innerText += button.textContent;
   });
 }
